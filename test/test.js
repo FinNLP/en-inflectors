054f677642759247dc0521b2ef7b4da0d0ac3154
Alex Corvi <alex@arrayy.com>
2017-01-04 10:33:03 +0300

    added more test cases

--- test/test.js
+++ test/test.js
@@ -2,62 +2,85 @@ const inflectors = require("../inflectors.js");
 const assert = require("assert");
 
 describe('singularization & pluralization', function () {
-	const singularsAndPluralsList = require("./singulars_plurals.json");
-
-	describe('booleans', function () {
-		
-		describe('isSingular', function () {
-			singularsAndPluralsList.forEach((arr)=>{
-				it('isSingular: '+arr[0], function () {
-					assert.equal(inflectors.isSingular(arr[0]),true);
-				});
+	const singulars = require("./singulars.json");
+	describe('test against huge list of common singulars', function () {
+		singulars.forEach((item)=>{
+			it('test item: '+item+' is not plural', function () {
+				assert.equal(inflectors.isPlural(item),false);
 			});
-		});
-
-		describe('isSingular', function () {
-			singularsAndPluralsList.forEach((arr)=>{
-				it('isSingular: '+arr[1], function () {
-					assert.equal(inflectors.isSingular(arr[1]),false);
-				});
-			});
-		});
-
-		describe('isPlural', function () {
-			singularsAndPluralsList.forEach((arr)=>{
-				it('isPlural: '+arr[1], function () {
-					assert.equal(inflectors.isPlural(arr[1]),true);
-				});
-			});
-		});
-		
-		describe('isPlural', function () {
-			singularsAndPluralsList.forEach((arr)=>{
-				it('isPlural: '+arr[0], function () {
-					assert.equal(inflectors.isPlural(arr[0]),false);
-				});
+			it('test item: '+item+' is singular', function () {
+				assert.equal(inflectors.isSingular(item),true);
 			});
 		});
 	});
 
-	describe('transformations', function () {
-		describe('plural to singular', function () {
-			singularsAndPluralsList.forEach((arr)=>{
-				it(arr[1], function () {
-					assert.equal(inflectors.singularize(arr[1]),arr[0]);
+	const plurals = require("./plurals.json");
+	describe('test against huge list of common plurals', function () {
+		plurals.forEach((item)=>{
+			it('test item: '+item+' is plural', function () {
+				assert.equal(inflectors.isPlural(item),true);
+			});
+			it('test item: '+item+' is not singular', function () {
+				assert.equal(inflectors.isSingular(item),false);
+			});
+		});
+	});
+
+	describe('test against comparative list of singulars and plurals', function () {
+		const singularsAndPluralsList = require("./singulars_plurals.json");
+		describe('booleans', function () {
+			
+			describe('isSingular', function () {
+				singularsAndPluralsList.forEach((arr)=>{
+					it('isSingular: '+arr[0], function () {
+						assert.equal(inflectors.isSingular(arr[0]),true);
+					});
+				});
+			});
+
+			describe('isSingular', function () {
+				singularsAndPluralsList.forEach((arr)=>{
+					it('isSingular: '+arr[1], function () {
+						assert.equal(inflectors.isSingular(arr[1]),false);
+					});
+				});
+			});
+
+			describe('isPlural', function () {
+				singularsAndPluralsList.forEach((arr)=>{
+					it('isPlural: '+arr[1], function () {
+						assert.equal(inflectors.isPlural(arr[1]),true);
+					});
+				});
+			});
+			
+			describe('isPlural', function () {
+				singularsAndPluralsList.forEach((arr)=>{
+					it('isPlural: '+arr[0], function () {
+						assert.equal(inflectors.isPlural(arr[0]),false);
+					});
 				});
 			});
 		});
-		describe('singular to plural', function () {
-			singularsAndPluralsList.forEach((arr)=>{
-				it(arr[0], function () {
-					assert.equal(inflectors.pluralize(arr[0]),arr[1]);
+
+		describe('transformations', function () {
+			describe('plural to singular', function () {
+				singularsAndPluralsList.forEach((arr)=>{
+					it(arr[1], function () {
+						assert.equal(inflectors.singularize(arr[1]),arr[0]);
+					});
+				});
+			});
+			describe('singular to plural', function () {
+				singularsAndPluralsList.forEach((arr)=>{
+					it(arr[0], function () {
+						assert.equal(inflectors.pluralize(arr[0]),arr[1]);
+					});
 				});
 			});
 		});
 	});
 });
-
-
 
 const verbsList = require("./verbs.json");
 function testConjugations(entry,expected){
